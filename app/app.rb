b5e21f38d4e25ef39a0d75d07877ebc26f269afe
Lucas Luz <lucas@calazansluz.com>
2023-04-10 00:19:42 +0200

    output file creation

--- app/app.rb
+++ app/app.rb
@@ -1,14 +1,24 @@
 require 'xmlsimple'
 require 'JSON'
-  config = XmlSimple.xml_in('app/menus/coffee.xml',
-    'KeyAttr'    => { 'item' => 'name' },
-    'ForceArray' => false,
-    'ContentKey' => '-content'
+
+
+
+config = XmlSimple.xml_in('app/menus/coffee.xml',
+  'KeyAttr'    => { 'item' => 'name' },
+  'ForceArray' => false,
+  'ContentKey' => '-content'
   )
-p config.class
+
+p
+# p config.class
 p '-\\-'
-p config['coffeeDrinks']['drink']
+# p config['coffeeDrinks']['drink']
 p '-\\-'
-p config['id'].to_json
+# p config['id'].to_json
 p '-\\-'
-p config['coffeeDrinks'].to_json
+# p JSON.pretty_generate(config)
+config['coffeeDrinks'].to_json
+p "Creating json file in app/menus directory"
+sleep(3)
+File.write("app/menus/#{config['id']}.json", JSON.pretty_generate(config))
+p "Json file of the menu id: #{config['id']} is ready."
