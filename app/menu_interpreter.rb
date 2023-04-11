914defe20945ad23357e910b2508592ea35064cd
Lucas Luz <lucas@calazansluz.com>
2023-04-11 15:42:53 +0200

    implemented the class and handled most possible errors

--- app/menu_interpreter.rb
+++ app/menu_interpreter.rb
@@ -1,39 +1,66 @@
 require 'xmlsimple'
 require 'JSON'
 
-def file_interpreter
-  puts "You initiate this file with the parameter #{ARGV[0]}"
+# This class will handle the xml and output a json file
+class MenuInterpreter
+  class BlankFilenameError < StandardError; end
+  class ExtensionNotProvided < StandardError; end
+  class ExtensionNotSupported < StandardError; end
+  class MissingMenuId < StandardError; end
 
-  @file_name = ARGV[0]
+  # initialize the class and checks for file name
+  def initialize(file_name)
+    raise BlankFilenameError, 'Filename is required for this action' if file_name.nil?
 
-  @file_extension = File.extname(@file_name)
+    puts "You called this file with the parameter #{ARGV[0]}"
+    @file_name = file_name
+  end
 
-  if @file_extension == '.xml'
-    puts "The file extension for the file you passed is #{@file_extension}"
-    file_parser
-  else
-    puts "File extension should be '.xml'"
+  # check if file extension is present and is .xml
+  def extension
+    file_extension = File.extname(@file_name)
+    raise ExtensionNotProvided, 'The filename provided has no extension' if file_extension.empty?
+
+    raise ExtensionNotSupported, 'The filename provided has extension that is not supported' if file_extension != '.xml'
+
+    file_extension
+  end
+
+  # parse file to array and checks for id
+  def parser
+    menu = XmlSimple.xml_in(
+      ARGV[0],
+      'KeyAttr' => { 'item' => 'name' },
+      'ForceArray' => false,
+      'ContentKey' => '-content'
+      )
+    menu_keys = menu.keys
+    raise MissingMenuId, 'The file structure is missing an id' unless menu_keys.include?('id')
+
+    # file_generator(menu, menu_keys)
+    structure(menu, menu_keys)
+  end
+
+  # idea to check structure, probably there's a better way of doing it... might not go down this road
+  def structure(menu, menu_keys)
+    p 'menu'
+    p menu
+    p 'menu_id'
+    p menu_id = menu_keys[0]
+    p 'menu_type'
+    p menu_type = menu_keys[1]
+    p 'menu_item'
+    p menu_item = menu[menu_type].keys[0]
+    p 'menu[menu_type][menu_item]'
+    p menu[menu_type][menu_item][0]
+    p menu[menu_type][menu_item][0].keys
+    p menu[menu_type][menu_item][0].keys.include?('name')
+    p menu[menu_type][menu_item][0].class
+    p menu[menu_type][menu_item][0].include?(:name)
+    # p menu[menu_keys[1]].keys
   end
 end
 
-def file_parser
-  menu = XmlSimple.xml_in(ARGV[0],
-    'KeyAttr'    => { 'item' => 'name' },
-    'ForceArray' => false,
-    'ContentKey' => '-content'
-    )
-    menu_keys = menu.keys
-    if menu_keys.include?('id')
-      file_generator(menu, menu_keys)
-    else
-      puts "Menu shoul have an id"
-    end
-end
-
-def file_generator(menu, menu_keys)
-  gen_file_name = "app/menus/#{menu_keys[1].downcase}_#{menu['id']}.json"
-  File.write(gen_file_name, JSON.pretty_generate(menu))
-  puts "The file '#{gen_file_name}' was generated"
-end
-
-file_interpreter
+menu_interpreter = MenuInterpreter.new(ARGV[0])
+menu_interpreter.extension
+menu_interpreter.parser
