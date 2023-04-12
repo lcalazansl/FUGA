c02c64172fce5935aa787e2883ede1c0198dfe07
Lucas Luz <lucas@calazansluz.com>
2023-04-12 23:32:33 +0200

    refactoring constants/private methods and more

--- app/menu_interpreter.rb
+++ app/menu_interpreter.rb
@@ -1,65 +1,66 @@
 require 'xmlsimple'
-require 'JSON'
+require 'json'
 
-# This class will handle the xml and output a json file
+# This class handles the conversion of XML menus to JSON files
 class MenuInterpreter
-  class BlankFilePathError < StandardError; end
-  class ExtensionNotProvided < StandardError; end
-  class ExtensionNotSupported < StandardError; end
-  class MissingMenuId < StandardError; end
-  class FileCouldNotBeFound < StandardError; end
+  SUPPORTED_EXTENSIONS = ['.xml'].freeze
+  MENU_DIR = 'app/menus/'.freeze
 
-  # initialize the class and checks for file name
+  class Error < StandardError; end
+  class BlankFilePathError < Error; end
+  class FileCouldNotBeFound < Error; end
+  class ExtensionError < Error; end
+  class MissingMenuId < Error; end
+
   def initialize(file_path)
     raise BlankFilePathError, 'File path is required for this action' if file_path.nil?
     raise FileCouldNotBeFound, 'File path could not be found' unless File.exist?(file_path)
 
-    puts "Executing interpreter for file #{ARGV[0]}"
     @file_path = file_path
   end
 
-  # check if file extension is present and is .xml
-  def extension
-    file_extension = File.extname(@file_path)
+  def parse
+    validate_file_extension
 
-    raise ExtensionNotProvided, 'The filename provided has no extension' if file_extension.empty?
-
-    raise ExtensionNotSupported, 'The filename provided has extension that is not supported' if file_extension != '.xml'
-  end
-
-  # parse file to array and checks for id
-  def parser
-
-    extension
-
-    # this method uses xmlsimple gem
     menu = XmlSimple.xml_in(
-      @file_path,
-      'KeyAttr' => { 'item' => 'name' },
-      'ForceArray' => false,
-      'ContentKey' => '-content'
+      @file_path, {
+        'KeyAttr' => { 'item' => 'name' },
+        'ForceArray' => false,
+        'ContentKey' => '-content'
+      }
     )
 
-    # this piece of code checks if the input file has an id
-    raise MissingMenuId, 'The file structure is missing an id' unless menu.keys.include?('id')
+    validate_menu_id(menu)
 
-    FileGenerator.new.json_generator(menu)
+    generate_json_file(menu)
   end
 
-end
+  private
 
-# Class function is responsible to generate json file from parsed xml file
-class FileGenerator
-  def json_generator(menu)
-    # generating json file name based on menu type + menu id
-    json_file_name = "#{menu.keys[1]}#{menu['id']}.json"
-    json_file_path = "app/menus/#{json_file_name}"
+  def validate_file_extension
+    extension = File.extname(@file_path)
 
-    puts "#{File.exist?(json_file_path) ? 'Updating' : 'Generating'} file '#{json_file_name}"
+    raise ExtensionError, 'The filename provided has no extension' if extension.empty?
+    raise ExtensionError, 'The filename provided has extension that is not supported' unless SUPPORTED_EXTENSIONS.include?(extension)
+  end
+
+  def validate_menu_id(menu)
+    raise MissingMenuId, 'The file structure is missing an id' unless menu.keys.include?('id')
+  end
+
+  def generate_json_file(menu)
+    menu_type = menu.keys[1]
+    menu_id = menu['id']
+    json_file_name = "#{menu_type}#{menu_id}.json"
+    json_file_path = "#{MENU_DIR}#{json_file_name}"
+
+    puts "#{File.exist?(json_file_path) ? 'Updating' : 'Generating'} file '#{json_file_name}'"
+
     File.write(json_file_path, JSON.pretty_generate(menu))
+
     puts "Output file: #{json_file_path}"
   end
 end
 
-menu_interpreter = MenuInterpreter.new(ARGV[0])
-menu_interpreter.parser
+# Usage: $ ruby menu_interpreter.rb input_file_path.xml
+MenuInterpreter.new(ARGV[0]).parse
